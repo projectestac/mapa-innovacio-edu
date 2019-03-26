05b768b8cc367547259b68f4093101ea63a1e8de
Francesc Busquets <francesc@gmail.com>
2019-03-26 19:26:32 +0100

    Initial commit of PHP site

---
+++ php-site/0_connexio.php
@@ -0,0 +1,13 @@
+<?php
+
+ // Database connection
+$servidor=@$_SERVER["SERVER_NAME"];
+$mpas= false;
+
+define("EW_CONN_HOST", "localhost", TRUE);
+define("EW_CONN_PORT", 3306, TRUE);
+define("EW_CONN_USER", "innovacio", TRUE);
+define("EW_CONN_PASS", "lstp2012", TRUE);
+define("EW_CONN_DB", "innovacio", TRUE);
+
+?>
