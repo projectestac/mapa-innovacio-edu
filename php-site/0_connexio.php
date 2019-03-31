3f1dd0f954eab270f79d27729fde7d7c024c1abd
Francesc Busquets <francesc@gmail.com>
2019-03-31 11:16:43 +0200

    Adapted for devilbox

--- php-site/0_connexio.php
+++ php-site/0_connexio.php
@@ -4,10 +4,17 @@
 $servidor=@$_SERVER["SERVER_NAME"];
 $mpas= false;
 
-define("EW_CONN_HOST", "localhost", TRUE);
+// ADAPTAT PER A DEVILBOX
+
+//define("EW_CONN_HOST", "localhost", TRUE);
+define("EW_CONN_HOST", "mysql", TRUE);
+
 define("EW_CONN_PORT", 3306, TRUE);
 define("EW_CONN_USER", "innovacio", TRUE);
-define("EW_CONN_PASS", "lstp2012", TRUE);
+
+//define("EW_CONN_PASS", "lstp2012", TRUE);
+define("EW_CONN_PASS", "innovacio", TRUE);
+
 define("EW_CONN_DB", "innovacio", TRUE);
 
 ?>
