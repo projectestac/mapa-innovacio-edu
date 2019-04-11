fe4285ade9b5eb19c516d0a0bd76e8457b845579
Francesc Busquets <francesc@gmail.com>
2019-04-11 15:38:43 +0200

    Read layer type from .env

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -3,6 +3,8 @@ import { Map, Polygon, Marker, Popup } from 'react-leaflet';
 import TileLayer from '../utils/TileLayer';
 import MarkerClusterGroup from '../utils/MarkerClusterGroup';
 
+// See ../utils/TileLayer for all available options
+const TILE_LAYER = process.env.REACT_APP_TILE_LAYER || 'wikimedia';
 
 export default function MainMap({ points = [], polygons = [], center = [41.7, 1.8], zoom = 8, maxZoom = 19, isCentre = false, updateMainState }) {
 
@@ -14,7 +16,7 @@ export default function MainMap({ points = [], polygons = [], center = [41.7, 1.
 
   return (
     <Map className={`${isCentre ? 'mapa-centre' : 'mapa'} markercluster-map`} {...{ center, zoom, maxZoom }}>
-      <TileLayer />
+      <TileLayer type={TILE_LAYER} />
       {polygons.map((p, n) => (
         <Polygon
           key={n}
--- src/utils/TileLayer.js
+++ src/utils/TileLayer.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { TileLayer } from 'react-leaflet';
 
-// Original OpenStreetMap
-const osm = <TileLayer
-  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-/>;
+const layers = {
+   // Original OpenStreetMap
+   osm: <TileLayer
+    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />,
+  // Mapbox.com
+  // ATENCIÓ: Max. 50.000 visualitzacions / mes
+  // Veure https://mapbox.com
+  // Provar amb id: `mapbox.satellite`, `mapbox.streets`...
+  mapbox: <TileLayer
+    attribution='Dades del mapa © contribuïdors de <a href="https://www.openstreetmap.org/">OpenStreetMap</a>, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imatges © <a href="https://www.mapbox.com/">Mapbox</a>'
+    url="https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}"
+    id='mapbox.streets'
+    accessToken="pk.eyJ1IjoiZnJuY2VzYyIsImEiOiJjanU1b3BkZ2owYThsNDR0M3A2MzBjczg2In0.5zKGaoSJeQOeiWZyOj8ByQ"
+  />,
+  // Wikimedia
+  wikimedia: <TileLayer
+    attribution='<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>'
+    url="https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png"
+    minZoom={1}
+    maxZoom={19}
+  />,
 
-// Mapbox.com
-// ATENCIÓ: Max. 50.000 visualitzacions / mes
-// Veure https://mapbox.com
-// Provar amb id: `mapbox.satellite`, `mapbox.streets`...
-const mapbox = <TileLayer
-  attribution='Dades del mapa © contribuïdors de <a href="https://www.openstreetmap.org/">OpenStreetMap</a>, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imatges © <a href="https://www.mapbox.com/">Mapbox</a>'
-  url="https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}"
-  id='mapbox.streets'
-  accessToken="pk.eyJ1IjoiZnJuY2VzYyIsImEiOiJjanU1b3BkZ2owYThsNDR0M3A2MzBjczg2In0.5zKGaoSJeQOeiWZyOj8ByQ"
-/>;
+}
 
-export default function ({ type = 'mapbox' }) {
-  return type === 'mapbox' ? mapbox : osm;
+export default function ({ type = 'wikimedia' }) {
+  return layers[type] || layers.wikimedia;
 }
