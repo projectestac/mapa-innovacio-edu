6c466d9021a62d398a45ce70fba924a66e7a0d58
Francesc Busquets <francesc@gmail.com>
2019-05-26 20:50:05 +0200

    CSV export

--- src/components/FitxaZona.js
+++ src/components/FitxaZona.js
@@ -17,7 +17,50 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { getInfoSpan, hasExtraInfo } from '../utils/Utils';
+import DownloadIcon from '@material-ui/icons/CloudDownload';
+import { getInfoSpan, hasExtraInfo, csvExportToFile } from '../utils/Utils';
+
+
+/**
+ * Export the list of schools to a CSV spreadsheet
+ */
+function exportData(programesArray, centresInn, nomcurt) {
+
+  const fields = [
+    { name: 'CODI', id: 'codi' },
+    { name: 'CENTRE', id: 'centre' },
+    { name: 'CURS', id: 'curs' },
+    { name: 'PROGRAMA', id: 'programa' },
+    { name: 'TITOL', id: 'titol' },
+    { name: 'INFO', id: 'url' },
+  ];
+
+  const base = `${window.location.origin}${window.location.pathname}#`;
+
+  const data = programesArray.reduce((total, programa) => {
+    const { centres, info } = programa;
+    return Object.keys(centres).reduce((result, curs) => {
+      centres[curs].filter(c => centresInn.has(c)).forEach(centre => {
+        const inf = info && info[centre.id] && info[centre.id].find(i => i.curs === curs);
+        result.push({
+          centre: `${centre.nom} (${centre.municipi})`,
+          codi: centre.id,
+          curs,
+          programa: programa.nom,
+          titol: inf ? inf.titol : '',
+          url: inf ? `${base}/projecte/${programa.id}|${centre.id}|${inf.num || 0}` : '',
+        });
+      });
+      return result;
+    }, total);
+  }, []);
+
+  return csvExportToFile(
+    `programes-${nomcurt.trim().replace(/ /g, '_')}.csv`,
+    data,
+    fields,
+  );
+}
 
 
 function FitxaZona({ history, match: { params: { key } } }) {
@@ -31,7 +74,7 @@ function FitxaZona({ history, match: { params: { key } } }) {
           return <Error {...{ error: `No hi ha cap zona amb el codi: ${key}`, history }} />
 
         // Els camps id, nomCurt i color no s'utilitzen
-        const { tipus, nom, logo, cp, adreca, municipi, comarca, tel, fax, correu, web, centresInn, programes } = zona;
+        const { tipus, nom, nomcurt, logo, cp, adreca, municipi, comarca, tel, fax, correu, web, centresInn, programes } = zona;
         const programesArray = Array.from(programes).sort((a, b) => a.nom.localeCompare(b.nom));
 
         const torna = () => history.goBack();
@@ -116,6 +159,17 @@ function FitxaZona({ history, match: { params: { key } } }) {
                     </ExpansionPanel>
                   )
                 })}
+                <br />
+                <Button
+                  variant="contained"
+                  className="csv-btn"
+                  title='Descarrega la llista de programes i centres en format CSV'
+                  onClick={() => exportData(programesArray, centresInn, nomcurt)}
+                >
+                  <DownloadIcon className="left-icon" />
+                  CSV
+                </Button>
+
               </Paper>
             </section>
             <MapSection {...{ data, programa: null, centre: null, zona: key, cursos, currentPrograms, polygons, mapChanged, updateMap }} />
