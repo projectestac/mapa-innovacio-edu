491fb74d0222b145ccfdd4238923ce86da5ebd36
Francesc Busquets <francesc@gmail.com>
2019-06-04 13:23:05 +0200

    Bulk loading of map markers

--- src/utils/MarkerClusterGroup.js
+++ src/utils/MarkerClusterGroup.js
@@ -4,7 +4,8 @@
 import { MapLayer, withLeaflet } from 'react-leaflet';
 import L from 'leaflet';
 require('leaflet.markercluster');
-// Moved to `index.js`
+
+// CSS imports moved to `../index.js`
 // import 'leaflet.markercluster/dist/MarkerCluster.css';
 // import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
@@ -12,23 +13,20 @@ require('leaflet.markercluster');
 // Based on: https://github.com/olabalboa/react-leaflet-markercluster/blob/master/src/react-leaflet-markercluster.js
 // See: https://github.com/YUzhva/react-leaflet-markercluster/pull/86
 L.MarkerClusterGroup.include({
-  _flushLayerBuffer() {
-    this.addLayers(this._layerBuffer);
-    this._layerBuffer = [];
-  },
   addLayer(layer) {
-    if (this._layerBuffer.length === 0)
-      setTimeout(this._flushLayerBuffer.bind(this), 0);
-    this._layerBuffer.push(layer);
+    if (!this._layerBuffer) {
+      this._layerBuffer = new Set();
+      setTimeout(() => {
+        if (this._layerBuffer && this._layerBuffer.size)
+          this.addLayers(Array.from(this._layerBuffer), true);
+        this._layerBuffer = null;
+      }, 0);
+    }
+    this._layerBuffer.add(layer);
   },
-});
-
-L.MarkerClusterGroup.addInitHook(function() {
-  this._layerBuffer = [];
 });
 
 class MarkerClusterGroup extends MapLayer {
-
   createLeafletElement(props) {
     const { clusterProps = {} } = props;
     const el = new L.markerClusterGroup({ ...props, ...clusterProps });
