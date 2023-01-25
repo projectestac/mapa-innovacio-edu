3bd383a9b1ee7351c1c07a40f5e2a9ac59d7b213
Francesc Busquets <francesc@gmail.com>
2023-01-25 13:31:40 +0100

    Update data

--- utils/update-data.sh
+++ utils/update-data.sh
@@ -6,7 +6,7 @@ echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "INFO: Collecting fresh data"
-  wget -O "dades-centres.csv" "https://docs.google.com/spreadsheets/d/e/2PACX-1vT7QVrFcb6c42LesPCMQvrrDoEWBAhZIBQoQTYfVZ6XcUqaJ_Rd7Uj9dtPY5EfGavBNy1P8Lyuj-2WV/pub?gid=1542544478&single=true&output=csv"
+  wget -O "dades-centres.csv" "https://docs.google.com/spreadsheets/d/e/2PACX-1vRpdo7gw7chsCUSO7vbfoJxjCxviGKOVnO_4QOJOYy7eQ_2_ocsQB4eT46KFlnLZLEcrAJAZy9wfvBF/pub?gid=1052887050&single=true&output=csv"
   wget -O "programes.csv" "https://docs.google.com/spreadsheets/d/e/2PACX-1vQltu0ck-HrUcJKN_-5wbsKW84LwnSZn9zIUZoRA4JGtE2fRqEbrdVn6KzjQWtCsja3MMEeNcS5yIAg/pub?gid=0&single=true&output=csv"
   wget -O "zones.csv" "https://docs.google.com/spreadsheets/d/e/2PACX-1vQltu0ck-HrUcJKN_-5wbsKW84LwnSZn9zIUZoRA4JGtE2fRqEbrdVn6KzjQWtCsja3MMEeNcS5yIAg/pub?gid=1406508845&single=true&output=csv"
   wget -O "instancies.csv" "https://docs.google.com/spreadsheets/d/e/2PACX-1vQltu0ck-HrUcJKN_-5wbsKW84LwnSZn9zIUZoRA4JGtE2fRqEbrdVn6KzjQWtCsja3MMEeNcS5yIAg/pub?gid=836258250&single=true&output=csv"
